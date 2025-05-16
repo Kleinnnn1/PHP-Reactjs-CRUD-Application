aadf18fda35f54e3aa0642c39964968b5d1724bb
-- <kennbalino@gmail.com>
2025-05-17 06:48:17 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+function App() {
+  return(
+    <div className='container'>
+      <h1 className='mt-5 mb-5 text-center'><b>PHP React.js + CRUD Application</b></h1>
+    </div>
+  )
+
+}
+
+export default App
